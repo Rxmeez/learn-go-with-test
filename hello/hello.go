7366fa66b59892e2dae7fa9e57faeab4a428aac7
Rameez Khan <rameez@mobkoi.com>
2020-11-09 08:30:00 +0000

    Refactored Add greetingPrefix function

--- hello/hello.go
+++ hello/hello.go
@@ -8,21 +8,24 @@ const frenchHelloPrefix = "Bonjour, "
 const spanish = "Spanish"
 const french = "French"
 
-func Hello(name, language string) string {
-	if name == "" {
-		name = "World"
-	}
-
-	prefix := englishHelloPrefix
+func greetingPrefix(language string) (prefix string) {
 
 	switch language {
 	case french:
 		prefix = frenchHelloPrefix
 	case spanish:
 		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-	return prefix + name
+	return
+}
 
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	return greetingPrefix(language) + name
 }
 
 func main() {
