e51dbdfd3b6ff5e10da3b526189251f383c543cd
rameez <rameez.rk94@gmail.com>
2020-11-12 22:44:37 +0000

    Error capture, in funcs and tests

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -16,6 +16,24 @@ func TestWallet(t *testing.T) {
 		}
 	}
 
+	assertError := func(t *testing.T, got error, want error) {
+		t.Helper()
+		if got == nil {
+			t.Fatal("didn't get an error but wanted one")
+		}
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	}
+
+	assertNoError := func(t *testing.T, err error) {
+		t.Helper()
+		if err != nil {
+			t.Fatal("did get an error but didn't wanted one")
+		}
+	}
+
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 
@@ -27,9 +45,20 @@ func TestWallet(t *testing.T) {
 	t.Run("Withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
 
-		wallet.Withdraw(Bitcoin(10))
+		err := wallet.Withdraw(Bitcoin(10))
 
 		assertBalance(t, wallet, Bitcoin(10))
+		assertNoError(t, err)
+	})
+
+	t.Run("Withdraw insufficient funds", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(10)}
+
+		err := wallet.Withdraw(Bitcoin(20))
+
+		assertBalance(t, wallet, Bitcoin(10))
+		assertError(t, err, ErrInsufficientFunds)
+
 	})
 
 }
