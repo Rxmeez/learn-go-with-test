cef83e9bb4a5780f4e4e042443f2091e6a3e4b85
Rameez Khan <rameez@mobkoi.com>
2020-11-16 22:48:23 +0000

    goroutines + channels

--- concurrency/check_websites.go
+++ concurrency/check_websites.go
@@ -2,14 +2,28 @@ package concurrency
 
 type WebsiteChecker func(string) bool
 
+type result struct {
+	string
+	bool
+}
+
 // CheckWebsites returns map of each url to boolean value
 // true for good response, false for bad response
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChannel := make(chan result)
 
 	for _, url := range urls {
-		results[url] = wc(url)
+		go func(u string) {
+			// Send statement
+			resultChannel <- result{u, wc(u)}
+		}(url)
 	}
 
+	for i := 0; i < len(urls); i++ {
+		// Receive expression
+		result := <-resultChannel
+		results[result.string] = result.bool
+	}
 	return results
 }
